a534f2ff3ff41fd4f023b80141f990cbbc6897f4
moonshadow565 <moonshadow565@hotmail.com>
2021-05-05 20:50:45 +0200

    add update from CDTB script

---
+++ update_from_CDTB.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+curl -L -o hashes/hashes.binentries.txt https://github.com/CommunityDragon/CDTB/raw/master/cdragontoolbox/hashes.binentries.txt
+
+curl -L -o hashes/hashes.binhashes.txt https://github.com/CommunityDragon/CDTB/raw/master/cdragontoolbox/hashes.binhashes.txt
+
+curl -L -o hashes/hashes.game.txt https://github.com/CommunityDragon/CDTB/raw/master/cdragontoolbox/hashes.game.txt
+
+curl -L -o hashes/hashes.lcu.txt https://github.com/CommunityDragon/CDTB/raw/master/cdragontoolbox/hashes.lcu.txt
